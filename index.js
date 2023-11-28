b38143bf3858de31a50e8c623779fec56190374c
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-11-28 20:55:51 +0700

    add sentry and methodOverride for PUT

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const Sentry = require('@sentry/node');
+const { ProfilingIntegration } = require('@sentry/profiling-node');
 const app = express();
 const PORT = 3000;
 const routers = require('./router');
@@ -9,9 +12,11 @@ const flash = require('express-flash');
 // const passport = require('./utils/passport');
 const swaggerJSON = require('./openapi.json');
 const swaggerUI = require('swagger-ui-express');
+const methodOverride = require('method-override');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(
   session({
     secret: 'keyboard cat',
@@ -26,11 +31,34 @@ app.use(flash());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './app/view'));
 app.use(cors());
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 app.get('/docs', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
 });
 app.use(routers);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + '\n');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
