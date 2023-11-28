99dd636634c2d2007355b1e1e23ad4b16108f175
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-11-28 20:56:33 +0700

    add mailer

---
+++ utils/mailer.js
@@ -0,0 +1,99 @@
+const nodeMailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+
+const transporter = nodeMailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USERNAME,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
+
+const sendEmail = async (toEmail, subject, content, isHtml) => {
+  const mailOptions = {
+    from: process.env.GMAIL_USERNAME,
+    to: toEmail,
+    subject,
+    [isHtml ? 'html' : 'text']: content,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+const sendResetPasswordEmail = async (toEmail, resetToken, name) => {
+  try {
+    const templatePath = path.join(__dirname, '../app/view/resetPassword.ejs');
+
+    ejs.renderFile(
+      templatePath,
+      {
+        name,
+        url: `${BASE_URL}/reset-password?email=${toEmail}&token=${resetToken}`,
+      },
+      (err, renderedTemplate) => {
+        if (err) {
+          console.error('Error rendering template:', err);
+          return;
+        }
+
+        sendEmail(toEmail, 'Password Reset', renderedTemplate, true);
+      }
+    );
+  } catch (error) {
+    console.error('Error sending password reset email:', error);
+  }
+};
+
+const sendRegistrationEmail = async (toEmail, name) => {
+  try {
+    const templatePath = path.join(__dirname, '../app/view/success.ejs');
+
+    ejs.renderFile(
+      templatePath,
+      {
+        name,
+      },
+      (err, renderedTemplate) => {
+        if (err) {
+          console.error('Error rendering template:', err);
+          return;
+        }
+
+        sendEmail(toEmail, 'Welcome to Big Tech', renderedTemplate, true);
+      }
+    );
+  } catch (error) {
+    console.error('Error sending registration email:', error);
+  }
+};
+
+const sendSuccessResetEmail = async (toEmail, name) => {
+  try {
+    const templatePath = path.join(__dirname, '../app/view/resetSuccess.ejs');
+
+    ejs.renderFile(
+      templatePath,
+      {
+        name,
+      },
+      (err, renderedTemplate) => {
+        if (err) {
+          console.error('Error rendering template:', err);
+          return;
+        }
+
+        sendEmail(toEmail, 'Password Reset Successful', renderedTemplate, true);
+      }
+    );
+  } catch (error) {
+    console.error('Error sending password reset successful email:', error);
+  }
+};
+
+module.exports = {
+  sendResetPasswordEmail,
+  sendRegistrationEmail,
+  sendSuccessResetEmail,
+};
--- utils/passport.js
+++ utils/passport.js
@@ -4,7 +4,6 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const { authUser } = require('../app/controllers/api/v1/auth');
-
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   done(null, await prisma.user.findUnique({ where: { id } }));
