5eef3d23e75167bafee21f6e7a4cedcd8843a232
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-11-28 20:59:00 +0700

    add routing for view ejs reset password

--- router/users.js
+++ router/users.js
@@ -4,6 +4,16 @@ const controller = require('../app/controllers');
 const { auth } = require('../utils/jwt');
 const multer = require('multer')();
 
+router.get('/mail-reset-password', (req, res) => {
+  res.render('forgotPassword.ejs');
+});
+router.get('/reset-password', (req, res) => {
+  const { email, token } = req.query;
+  res.render('newPassword.ejs', { email, token });
+});
+router.put('/v1/users/mail-reset-password', controller.users.mailResetPassword);
+router.put('/v1/users/reset-password', controller.users.resetPassword);
+
 router.get('/v1/users', controller.users.getUsers);
 router.post('/v1/users', multer.single('image'), controller.users.createUser);
 router.get('/v1/users/:id', auth, controller.users.getUserById);
@@ -15,7 +25,6 @@ router.put(
   multer.single('image'),
   controller.users.updateProfileAndImage
 );
-
 router.delete('/v1/profile', auth, controller.users.deleteProfileImage);
 
 module.exports = router;
